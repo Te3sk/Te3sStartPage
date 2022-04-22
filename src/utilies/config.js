2ccbce7a70bd2abcdee29055049bd2b0e4fab62a
Te3sk <101196863+Te3sk@users.noreply.github.com>
2022-04-22 23:19:57 +0200

    Add files via upload

---
+++ src/utilies/config.js
@@ -0,0 +1,59 @@
+export const conf = {
+    //General
+    "searcUsing": "google", // google, duckduckgo, custom
+    "darkMode" : true, //true, false
+    "theme" : "weed", // weed, sunset  -  other to dev
+    "themeDefault" : "weed",
+    "dinamicImage" : true, // inverse, have to correct
+    "newTab" : false,
+    "dateTime" : {
+        "hourFormat12" : false, // true, false
+        "wishes" : {
+            "set" : true, //true to see it
+            "morning" : "Good Morning", 
+            "afternoon" : "Good Afternoon",
+            "evening" : "Good Evening",
+            "night" : "Good Night",
+        },
+    },
+    "lists" : {
+        //set up max 6 groups and max 6 link for group
+        "Google Stuff" : {
+            "Google" : "https://www.google.com/",
+            "Drive" : "https://drive.google.com/drive/u/0/my-drive",
+            "Calendar" : "https://calendar.google.com/calendar/u/0/r",
+            "Maps" : "https://www.google.it/maps/",
+            "Translator" : "https://translate.google.it/",
+            "Contacts" : "https://contacts.google.com/?hl=it",
+        },
+        "Work" : {
+            "Zimbra Mail" : "https://posta.netresults.it/modern/email/Inbox/",
+            "NR Space" : "https://netresults.jetbrains.space/im/%23Spacebox",
+            "Twilio" : "https://console.twilio.com/us1/develop/studio/flows?frameUrl=%2Fconsole%2Fstudio%2Fflows%3Fx-target-region%3Dus1",
+        },
+        "University" : {
+            "Study Plan" : "https://www.unipi.it/index.php/lauree/regolamento/10298",
+            "Agenda Didattica" : "https://agendadidattica.unipi.it/Prod/Home/Calendar",
+            "Classroom" : "https://classroom.google.com/u/2/h/",
+            "Valutami" : "https://esami.unipi.it/archivioiscrizioni.php?",
+            "M Teems" : "https://teams.microsoft.com/_#/school/conversations/Generale?threadId=19:75b1b0c10fb543d08ca22b107596099e@thread.tacv2&ctx=channel",
+            "eLearning" : "https://elearning.di.unipi.it/",
+        },
+        "TV" : {
+            "Netflix" : "https://www.netflix.com/browse",
+            "Prime Video" : "https://www.primevideo.com/ref=av_auth_return_redir",
+            "Disney+" : "https://www.disneyplus.com/home",
+            "JustWatch" : "https://www.justwatch.com/it",
+            "RaiPlay" : "https://www.raiplay.it/"
+        },
+        "Other" : {
+            "GitHub" : "https://github.com/",
+            "Amazon" : "https://www.amazon.it/",
+            "Reddit" : "https://www.reddit.com/",
+            "OverLeaf" : "https://www.overleaf.com/project",
+            "HomeBanking" : "https://homebanking.bpergroup.net//wps/portal/hb/home/ibpr/sec/login/login?bank=05387&channel=IBPR&a=1&_ga=2.248626126.83323429.1609262983-1963081641.1609262983",
+            "Spotify" : "https://open.spotify.com/",
+        },
+        
+    },
+}
---
+++ src/utilies/themeConfig.js
@@ -0,0 +1,95 @@
+import WeedD from '../assets/gif/weed-din.gif'
+import WeedS from '../assets/img/joint-logo.png'
+import SunD from '../assets/gif/sunset.gif'
+import SunS from '../assets/img/sunsetStc.png'
+import MountainS from '../assets/img/mountainStc.png'
+import MountainD from '../assets/gif/mountainDin.gif'
+
+export const styles = {
+    "weed" : {
+        "dark" : {
+            "stcImg" : WeedS,
+            "dinImg" : WeedD,
+            "mainColor" : "green",
+            "secondColor" : "dark-green"
+        },
+        "normal" : {
+            "stcImg" : WeedS,
+            "dinImg" : WeedD,
+            "mainColor" : "green",
+            "secondColor" : "dirty-white"
+        },
+    },
+
+    "sunset" : {
+        "dark" : {
+            "stcImg" : SunS,
+            "dinImg" : SunD,
+            "mainColor" : "red",
+            "secondColor" : "black"
+        },
+        "normal" : {
+            "stcImg" : SunS,
+            "dinImg" : SunD,
+            "mainColor" : "red",
+            "secondColor" : "white"
+        },
+    },
+
+    "mountain" : { //dev
+        "dark" : {
+            "stcImg" : MountainS,
+            "dinImg" : MountainD,
+            "mainColor" : "montBlue",
+            "secondColor" : "black"
+        },
+        "normal" : {
+            "stcImg" : SunS,
+            "dinImg" : SunD,
+            "mainColor" : "red",
+            "secondColor" : "white"
+        },
+    },
+    "cinema" : {    //dev
+        "dark" : {
+            "stcImg" : SunS,
+            "dinImg" : SunD,
+            "mainColor" : "red",
+            "secondColor" : "black"
+        },
+        "normal" : {
+            "stcImg" : SunS,
+            "dinImg" : SunD,
+            "mainColor" : "red",
+            "secondColor" : "white"
+        },
+    },
+    "lake" : {  //dev
+        "dark" : {
+            "stcImg" : SunS,
+            "dinImg" : SunD,
+            "mainColor" : "red",
+            "secondColor" : "black"
+        },
+        "normal" : {
+            "stcImg" : SunS,
+            "dinImg" : SunD,
+            "mainColor" : "red",
+            "secondColor" : "white"
+        },
+    },
+    "boat" : {  //dev
+        "dark" : {
+            "stcImg" : SunS,
+            "dinImg" : SunD,
+            "mainColor" : "red",
+            "secondColor" : "black"
+        },
+        "normal" : {
+            "stcImg" : SunS,
+            "dinImg" : SunD,
+            "mainColor" : "red",
+            "secondColor" : "white"
+        },
+    },
+}
